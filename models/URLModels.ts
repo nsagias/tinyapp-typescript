afc96dbbfb4e5bf4e89de3fba81ed4da3996fa9d
Nick <nsagias@gmail.com>
2023-03-28 18:07:22 -0400

    refactor, change data shape, add type

---
+++ models/URLModels.ts
@@ -0,0 +1,34 @@
+import moment from "moment";
+
+
+let urlData = [
+  { 
+    shortenedURL : "b2xVn2",
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "1f1ffea1",
+    createdAt: moment().format('MMMM Do YYYY'),
+  },
+  { 
+    shortenedURL : "9sm5xk",
+    longURL: "http://www.google.com",
+    userID: "1f1ffea1",
+    createdAt: moment().format('MMMM Do YYYY'),
+  },
+  {
+    shortenedURL :"9sm511",
+    longURL: "http://www.bingo.com",
+    userID: "815bd08a",
+    createdAt: moment().format('MMMM Do YYYY'),
+  },
+  { 
+    shortenedURL :"c2k511",
+    longURL: "http://www.yahoo.com",
+    userID: "815bd08a",
+    createdAt: moment().format('MMMM Do YYYY')
+  }
+];
+
+
+module.exports = {
+  urlData,
+};
