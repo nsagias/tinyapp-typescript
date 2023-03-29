d17eab060233f903d8462e6ce69eb3def60a8959
Nick <nsagias@gmail.com>
2023-03-29 17:49:54 -0400

    add get urls for user

--- data/urlData.ts
+++ data/urlData.ts
@@ -8,10 +8,7 @@ import { IURLModel } from "../types/urlData";
 import { urlData } from "../models/URLModels";
 
 
-const getUrlsByUser = async (id: string): Promise<IURLModel[]> => {
+export const getUrlsByUserId = async (id: string): Promise<IURLModel[]> => {
   return await urlData.filter((u: any) => u.userID === id);
 };
 
-module.exports = {
-  getUrlsByUser
-};
--- routes/urlRoute.ts
+++ routes/urlRoute.ts
@@ -1,7 +1,26 @@
 import { Router, Request, Response } from "express";
+import { getUrlsByUserId }   from "../data/urlData";
 
 export const urlRoute = Router();
 
 urlRoute.get("/urls", (req: Request, res: Response) => {
   res.json({ message: "urls"});
+});
+
+
+urlRoute.get("/urls/:userId", async(req: Request, res: Response) => {
+  try {
+    const userId = req.params && req.params.userId || null;
+    const cookieUserId = req.cookies && req.cookies.userID || null;
+  
+    if (!userId) throw new Error("missing user params");
+    if (!cookieUserId) throw new Error("please authenticate")
+
+    const urls = await getUrlsByUserId(userId);
+    res.json({ data: urls });
+    
+  } catch (error) {
+    console.error(error);
+    res.json({ error: "error"});
+  }
 });
