dc2faf1ea44c0876ed1eb2bab1a0bb04b718e2af
Nick <nsagias@gmail.com>
2023-03-29 19:53:50 -0400

    fix uuid import to v4

--- services/utilsService.ts
+++ services/utilsService.ts
@@ -1,9 +1,9 @@
 
-import uuid from "uuid";
+import { v4 }from "uuid";
 /**
  * Function to create shortURL identifier for database insertion
  * @returns {string} 6 character long string aplhanumeric identifier
  */
-export const idGenerator = (len: number = 6): string => {
-  return uuid.v4().substring(0, len).toString();
+export const idGenerator = async(len: number = 6): Promise<string> => {
+  return await v4().substring(0, len).toString();
 };
