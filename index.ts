83a93ab475351f2d8e555501903f9c0413ac416c
Nick <nsagias@gmail.com>
2023-03-29 16:26:22 -0400

    refactor login

--- index.ts
+++ index.ts
@@ -5,9 +5,7 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import logger from "morgan";
-import { getUserByEmail } from "./data/userData";
-import { IUserModel } from "./types/user";
-import { checkPassword } from "./services/authService";
+
 import { routes } from './routes';
 
 dotenv.config();
@@ -43,27 +41,7 @@ app.use("/", routes);
 
 
 
-// app.post
-app.get("/login", async(req: Request, res: Response) => {
-  const myUserId = "815bd08a";
-  const user: IUserModel | undefined = await getUserByEmail("red@example.com");
-  const userAuth: boolean = await checkPassword("red@example.com", "abc123");
-  console.log("*USER",user)
-  console.log("***Auth:", userAuth)
-  await res.cookie("userID", myUserId);
-  await res.json({ name: "nick", userId: myUserId, bingo: user!.email, userAuth});
-});
 
-// app.post
-app.get("/authenticate", async(req: Request, res: Response) => {
-  const myUserId = "815bd08a";
-  const cookieCheck = req.cookies && req.cookies.userID || null;
-  if (cookieCheck === myUserId) {
-    res.json({ name: "nick"});
-  } else {
-    res.json(null);
-  }
-});
 
 
 // app.post("/login", (req: Request, res: Response) => {
--- routes/authRoute.ts
+++ routes/authRoute.ts
@@ -1,7 +1,35 @@
 import { Router, Request, Response } from "express";
+import { getUserByEmail } from "../data/userData";
+import { IUserModel } from "../types/user";
+import { checkPassword } from "../services/authService";
 
 export const authRoute = Router();
 
-authRoute.get("/authenticate", (req: Request, res: Response) => {
+authRoute.get("/auth", (req: Request, res: Response) => {
   res.json({ auth : "auth"});
 });
+
+
+// app.post
+authRoute.get("/login", async(req: Request, res: Response) => {
+  const myUserId = "815bd08a";
+  const user: IUserModel | undefined = await getUserByEmail("red@example.com");
+  const userAuth: boolean = await checkPassword("red@example.com", "abc123");
+  console.log("*USER",user)
+  console.log("***Auth:", userAuth)
+  await res.cookie("userID", myUserId);
+  await res.json({ name: "nick", userId: myUserId, bingo: user!.email, userAuth});
+});
+
+
+// app.post
+authRoute.get("/authenticate", async(req: Request, res: Response) => {
+  const myUserId = "815bd08a";
+  const cookieCheck = req.cookies && req.cookies.userID || null;
+  if (cookieCheck === myUserId) {
+    res.json({ name: "nick"});
+  } else {
+    res.json(null);
+  }
+});
+
