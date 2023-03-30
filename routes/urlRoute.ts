e4e78a698e46541635b250b372994b8fe1f537c2
Nick <nsagias@gmail.com>
2023-03-29 21:02:19 -0400

    add route to create new urls

--- routes/urlRoute.ts
+++ routes/urlRoute.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express";
-import { getURLByShortenedURL, getUrlsByUserId }   from "../data/urlData";
+import { createNewURL, getURLByShortenedURL, getUrlsByUserId }   from "../data/urlData";
 
 export const urlRoute = Router();
 
@@ -77,6 +77,34 @@ urlRoute.get("/url/:shortenedURL", async (req: Request, res: Response) => {
     // receives a shoten url from an anonymous user
     const longURLData = await getURLByShortenedURL(shortenedURL)
     if (!longURLData  || !(longURLData.length > 0)) throw new Error("URL does not exist");
+        
+    // TODO: add DTO
+    res.json({ message: "success", data: longURLData});
+
+  } catch (error: any) {
+    console.error(error);
+    res.json({ message: "error"});
+  }
+});
+
+/**
+ * create new url
+ */
+
+// urlRoute.post
+urlRoute.get("/url/new/:longURL", async (req: Request, res: Response) => {
+  try {
+    // TODO: update before
+    // const longURL = req.body && req.body.longURL || null;
+    const longURL = req.params && req.params.longURL || null;
+    const cookieUserId = req.cookies && req.cookies.userID || null;
+
+    if (!longURL)  throw new Error("please provide shortened Url");
+    if (!cookieUserId) throw new Error("please authenticate");
+    
+    // receives a shoten url from an anonymous user
+    const longURLData = await createNewURL(longURL, cookieUserId);
+    if (!longURLData ) throw new Error("URL was not created");
         
     // TODO: add DTO
     res.json({ message: "success", data: longURLData});
