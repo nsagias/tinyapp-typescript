6a44990b5cfee26428bb18a5348280eb6adc1500
Nick <nsagias@gmail.com>
2023-03-30 04:36:14 -0400

    remove unused imports

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
-
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
 import path from "path";
 import cookieParser from "cookie-parser";
