2fc96c1597b5a8d1cba49da8e92fa6c39c72a73d
Nick <nsagias@gmail.com>
2023-03-29 20:55:01 -0400

    add new function to create urls

--- data/urlData.ts
+++ data/urlData.ts
@@ -6,6 +6,8 @@
 
 import { IURLModel } from "../types/urlData";
 import { urlData } from "../models/URLModels";
+import { idGenerator } from "../services/utilsService";
+import moment from "moment";
 
 /**
  * Get urls by user id
@@ -23,4 +25,41 @@ export const getUrlsByUserId = async (id: string): Promise<IURLModel[]> => {
  */
 export const getURLByShortenedURL = async (shotenedURLId: string) => {
   return await urlData.filter((u: any) => u.shortenedURL === shotenedURLId);
-}
+};
+
+
+/**
+ * Create new short url for user
+ * @param longURL 
+ * @param userId 
+ * @returns 
+ */
+export const createNewURL = async (longURL: string, userId: string): Promise<IURLModel | string> => {
+  let newShortURLId = await idGenerator();
+  console.log("NEW GENERATED LINK", newShortURLId);
+
+  // check if already exists before generatoring
+  let isExistingURL = await getURLByShortenedURL(newShortURLId);
+
+  if (isExistingURL) {
+    // existing call new url with 8 letter
+    newShortURLId = await idGenerator(8);
+  }
+
+  const newURL: IURLModel | null = await { 
+    shortenedURL : newShortURLId,
+    longURL: longURL,
+    userID: userId,
+    createdAt: await moment().format('MMMM Do YYYY'),
+  };
+
+  if (newURL) {
+    await urlData.push(...urlData, newURL);
+    await console.log("CREATED SHORT URL", newURL);
+    await console.log("CREATED NEW USER", urlData);
+    return newURL;
+    // // return true;
+  }
+  // return false;
+  return "failed to create new url";
+};
