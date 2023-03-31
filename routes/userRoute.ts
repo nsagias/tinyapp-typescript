9b8195764a6a542fc91b848fa54afb661e37f247
Nick <nsagias@gmail.com>
2023-03-31 01:46:43 -0400

    remove dead path

--- routes/userRoute.ts
+++ routes/userRoute.ts
@@ -4,10 +4,6 @@ import { IUserModel } from "../types/user";
 
 export const userRoute = Router();
 
-userRoute.get("/user", (req: Request, res: Response) => {
-  res.json({ auth : "auth"});
-});
-
 // http://localhost:3001/users/1/?page=1&limit=10&search=:search
 userRoute.get("/users/:userId", (req: Request, res: Response) => {
   console.log("REQ.PARAM",req.params);
