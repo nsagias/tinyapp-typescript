0d4b549a9a677a6162e842140911d9f01efa2b77
Nick <nsagias@gmail.com>
2023-03-31 00:58:18 -0400

    refactor auth to loginRoutes

--- routes/authRoute.ts
+++ routes/authRoute.ts
@@ -1,57 +1,10 @@
 import { Router, Request, Response } from "express";
-import { getUserByEmail } from "../data/userData";
-import { IUserModel } from "../types/user";
-import { checkPassword } from "../services/authService";
 
 export const authRoute = Router();
 
-authRoute.get("/auth", (req: Request, res: Response) => {
-  res.json({ auth : "auth"});
-});
 
-
-// app.post
-authRoute.get("/login", async(req: Request, res: Response) => {
-  const myUserId = "815bd08a";
-  try {
-    // Check for body 
-    // const email = req.body && req.body.email || null;
-    // const password = req.body && req.body.password || null;
-    const email = "red@example.com";
-    const password = "abc123";
-    if (!email || !password) {
-      throw new Error("missing_email_or_password_1");
-    }
-
-    // check for empty strings
-    const parsedEmail = email.trim();
-    const parsedPassword = password.trim();
-
-    if (parsedEmail === "" || parsedPassword === "") {
-      throw new Error("missing_email_or_password_2");
-    }
-
-    const user: IUserModel | boolean = await getUserByEmail( parsedEmail || "red@example.com");
-    console.log("*USER",user);
-
-    const userAuth: boolean = await checkPassword(parsedEmail || "red@example.com", parsedPassword || "abc123");
-    console.log("***Auth:", userAuth);
-
-    if (!user) {
-      throw new Error("missing_email_or_password_3");
-    } else if (user) {
-      await res.cookie("userID", myUserId);
-      await res.json({ name: "nick", userId: myUserId, email: user, userAuth});
-    }
-        
-  } catch (error: any) {
-      console.error("ERROR",error);
-      return res.json({ message: "login error"})
-  }
-});
-
-
-// app.post
+// TODO: app.post
+// TODO: to update with token authentication
 authRoute.get("/authenticate", async(req: Request, res: Response) => {
   const myUserId = "815bd08a";
   const cookieCheck = req.cookies && req.cookies.userID || null;
@@ -63,14 +16,3 @@ authRoute.get("/authenticate", async(req: Request, res: Response) => {
 });
 
 
-/**
- * Logout
- */
-authRoute.post("/logout", async (req: Request, res: Response) => {
-  // set session value to null
-  if(req.cookies && req.cookies.userID) {
-    req.cookies = null;
-    res.json({ message: "success"});
-  }
-  res.json({ message: "does not exist"})
-});
--- routes/index.ts
+++ routes/index.ts
@@ -4,11 +4,13 @@ import { errorRoute } from "./errorRoute";
 import { rootRoute } from "./rootRoute";
 import { userRoute } from "./userRoute";
 import { urlRoute } from "./urlRoute";
+import { loginRoute } from "./loginRoute";
 
 export const routes = Router();
 
 routes.use(authRoute);
 routes.use(errorRoute);
+routes.use(loginRoute);
 routes.use(rootRoute);
 routes.use(userRoute);
 routes.use(urlRoute);
