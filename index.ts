23ba19c6e87ea33e33f422fca20c4c0495a8107f
Nick <nsagias@gmail.com>
2023-03-31 15:28:24 -0400

    lint index

--- index.ts
+++ index.ts
@@ -5,7 +5,6 @@ import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import logger from "morgan";
 import cors from "cors";
-
 import { routes } from './routes';
 
 dotenv.config();
