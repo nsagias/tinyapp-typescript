a8c08cb160a91bd8d013489cffe914e8eb910d3a
Nick <nsagias@gmail.com>
2023-04-01 20:29:04 -0400

    add create table for user_model

--- databases/sequelize/schemas/user_model.sql
+++ databases/sequelize/schemas/user_model.sql
@@ -1,6 +1,9 @@
-CREATE TABLE `tinyapp`.`user` (
-  `id` INT NOT NULL,
-  `name` VARCHAR(256) NOT NULL,
+CREATE TABLE `tinyapp`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(4) NOT NULL,
   `email` VARCHAR(256) NOT NULL,
   `password` VARCHAR(256) NULL,
-  PRIMARY KEY (`id`));
+  `createdAt` TIMESTAMP NULL,
+  `updatedAt` TIMESTAMP NULL,
+PRIMARY KEY (`id`));
+
