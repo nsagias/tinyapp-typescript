7a797eb43498eabb80f93b651796df0eb4c0048b
Nick <nsagias@gmail.com>
2023-04-01 20:29:38 -0400

    import dotenv

--- databases/sequelize/sequelize_config.ts
+++ databases/sequelize/sequelize_config.ts
@@ -1,25 +1,30 @@
-
 import { Dialect, Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const DB_USER: string | undefined = process.env.DB_USER || undefined;
+dotenv.config();
+
+const DB_USER: string | undefined = process.env.DB_USER;
 const DB_PORT: number| undefined  = parseInt(process.env.DB_PORT!, 10) || undefined;
 const DB_PASSWORD: string | undefined = process.env.DB_PASSWORD || undefined; 
-const DB_HOST: string | undefined = process.env.DB_HOST || undefined; 
+const DB_HOST: string | undefined = process.env.DB_HOST;
 const DB_NAME: string | undefined = process.env.DB_NAME || undefined; 
 const DB_DIALECT: Dialect | undefined = process.env.DB_DIALECT as Dialect || undefined; 
 
 
-export const sequelize = new Sequelize(DB_NAME!, DB_USER!, DB_PASSWORD,{
-  host: DB_HOST,
-  port: DB_PORT,
-  dialect: DB_DIALECT
-});
+export const sequelize = new Sequelize(
+  DB_NAME!, 
+  DB_USER!, 
+  DB_PASSWORD,
+    {
+      host: DB_HOST,
+      port: DB_PORT,
+      dialect: DB_DIALECT
+    }
+);
 
 try {
   sequelize.authenticate();
-  console.log("worked")
+  console.log("worked x")
 } catch (error) {
   console.log("DID NOT WORK")
 }
-
-
