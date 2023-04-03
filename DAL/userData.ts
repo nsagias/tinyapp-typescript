a4f40314dd1bf2b0b0627370d40ba2bdef9fecdb
Nick <nsagias@gmail.com>
2023-04-03 01:25:06 -0400

    update createUser, update getUserByEmail, add updateBy and get all users

--- DAL/userData.ts
+++ DAL/userData.ts
@@ -2,63 +2,69 @@ import bcrypt from "bcryptjs";
 import { users } from "../databases/userModelSeed";
 import { User } from "../models";
 import { IUser } from "../types/user";
-/**
- * Function to used to create a new user
- * @param {number} id
- * @param {string} name
- * @param {string} email
- * @param {string} password
- * @returns {array} enters the new user into the database
- */
-export const createUserMock = async (name: string, email:string, password: string): Promise<IUser | boolean> => {
-  let newUser: IUser | null = { 
-  
-    firstName: name, 
-    lastName: name, 
-    email: email, 
-    password: await bcrypt.hashSync(password, 10)
-  };
-  if (newUser) {
-    await users.push(...users, newUser);
-    await console.log("CREATED NEW DB", users);
-    await console.log("CREATED NEW USER", newUser);
-    return newUser;
-    // return true;
-  }
-  return false;
-};
 
-export const createUser = async (name: string, email: string, password: string): Promise<IUser | boolean> => {
-  
-  let newUser: any = { 
-    firstName: name, 
-    lastName: name, 
-    email: email, 
-    password: await bcrypt.hashSync(password, 10)
-  };
-  if (newUser) {
-    // await users.push(...users, newUser);
-    await console.log("CREATED NEW DB", users);
-    await console.log("CREATED NEW USER", newUser);
 
-    const createdUser: any = await User.create(newUser);
-    console.log("created user", createdUser);
-    return true;
-  }
-  return false;
+export const createUser = async (firstName: string, lastName: string, email: string, password: string, active: boolean): Promise<IUser | boolean> => {
+  
+  const newUser = {
+    firstName,
+    lastName,
+    email,
+    password: bcrypt.hashSync(password, 10),
+    active,
+    deletedAt: new Date()
+  } as User;
+
+  return await User.create(newUser);  
 };
 
 
 /**
- * Function to confirm user in database
- * used to avoid duplicate user creation
- * @param {string} userEmail
- * @returns {boolean or undefined} true if user in database or undefined
+ * 
+ * @param id 
+ * @param deletedDate 
+ * @param isActive 
+ * @returns 
  */
-export const getUserByEmail = async (userEmail: string): Promise<IUser | boolean> => {
-  const user: any =  await users.filter((user: IUser) => user.email === userEmail);
-  if (user) {
-    return user[0];
-  }
-  return false;
+export const getUserById = async (id: number, deletedDate: Date | undefined = undefined, isActive: boolean = true ): Promise<IUser | null> => {
+  return await User.findOne({ where: { id, deletedAt: deletedDate, active: isActive } });
+};
+
+/**
+ * 
+ * @param email 
+ * @param deletedDate 
+ * @param isActive 
+ * @returns 
+ */
+export const getUserByEmail = async (email: string, deletedDate: Date | undefined = undefined, isActive: boolean = true): Promise<IUser | null> => {
+  return await User.findOne({ where: { email, deletedAt: deletedDate, active: isActive  } });
+};
+
+
+/**
+ * 
+ * @param id 
+ * @param values 
+ * @returns 
+ */
+export const updateById = async (id: number, values: Partial<IUser>): Promise<boolean>  => {
+  const user = await User.findByPk(id);
+  if (!user) return false;
+
+  await user.set(values);
+  await user.save();
+
+  return true;
+};
+
+
+/**
+ * 
+ * @param deletedDate 
+ * @param isActive 
+ * @returns 
+ */
+export const getAllUsers = async (deletedDate: Date | undefined = undefined, isActive: boolean = true) => {
+  return await User.findAll({where: { deletedAt: deletedDate, active: isActive }});
 };
