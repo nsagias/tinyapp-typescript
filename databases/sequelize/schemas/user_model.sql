c249416cec51ee076410d2db5da936a7f5ff9c91
Nick <nsagias@gmail.com>
2023-04-02 20:47:04 -0400

    delete schema folder and user_model.sql

--- databases/sequelize/schemas/user_model.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `tinyapp`.`users` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `name` VARCHAR(4) NOT NULL,
-  `email` VARCHAR(256) NOT NULL,
-  `password` VARCHAR(256) NULL,
-  `createdAt` TIMESTAMP NULL,
-  `updatedAt` TIMESTAMP NULL,
-PRIMARY KEY (`id`));
-
