b4b72d908b687e82c4eac2109f618d01150252e6
Nick <nsagias@gmail.com>
2023-04-03 19:38:03 -0400

    create hashpassword and comparePassword and update checkpassword

--- services/authService.ts
+++ services/authService.ts
@@ -1,6 +1,30 @@
 import bcrypt from "bcryptjs";
 import { getUserByEmail } from "../DAL/userData";
 
+
+
+
+/**
+ * Password hashing function to encapsulate 
+ * @param password 
+ * @param rounds 
+ * @returns 
+ */
+export const hashPassword = async (password: string, rounds: number = 10): Promise<string> => {
+  return await bcrypt.hash(password, rounds);
+};
+
+/**
+ * Password compare to check if password send matches hashed password
+ * @param password 
+ * @param hashedPassword 
+ * @returns 
+ */
+export const comparePassword = async (password: string, hashedPassword: string): Promise<boolean> => {
+  return await bcrypt.compare(password, hashedPassword);
+};
+
+
 /**
  * Check password matches db
  * @param {string} email
@@ -9,12 +33,17 @@ import { getUserByEmail } from "../DAL/userData";
  */
 export const checkPassword = async (email: string, password: string): Promise<boolean> => {
   const user: any = await getUserByEmail(email);
-  if (!user) {
-    return false;
-  }
-  // TODO: to put insisde function, security spec
-  if (bcrypt.compareSync(password, user.password)) {
-   return true; 
-  }
+
+  // check if user exists
+  if (!user) return false;
+
+  // check if passwords match
+  const passwordsMatch = await comparePassword(password, user.password);
+  
+  if (passwordsMatch) return true; 
+  
   return false;
 };
+
+
+
