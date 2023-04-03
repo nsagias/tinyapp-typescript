41283b0bc3ac9291babcce551d35e4199abfb152
Nick <nsagias@gmail.com>
2023-04-03 04:01:34 -0400

    add createShortUrl dal function

--- DAL/urlData.ts
+++ DAL/urlData.ts
@@ -1,13 +1,16 @@
 import { IUrlModel } from "../types/urlModel";
-import { urlData } from "../models/UrlModel";
+import { urlData, UrlModel } from "../models/UrlModel";
 import { idGenerator } from "../services/utilsService";
 
 /**
  * Get urls by user id
  * @returns all urls for a user
  */
-export const getUrlsByUserId = async (userId: number): Promise<IUrlModel[]> => {
+export const getUrlsByUserIdMock = async (userId: number): Promise<IUrlModel[]> => {
   return await urlData.filter((u: any) => u.userID === userId);
+};
+export const getUrlsByUserId = async (userId: number): Promise<UrlModel[]> => {
+  return await UrlModel.findAll({ where : { userId } });
 };
 
 /**
@@ -15,8 +18,12 @@ export const getUrlsByUserId = async (userId: number): Promise<IUrlModel[]> => {
  * @param shotenedURLId
  * @returns 
  */
-export const getUrlByShortUrl = async (shotenedURLId: string) => {
+export const getUrlByShortUrlMock = async (shotenedURLId: string) => {
   return await urlData.filter((u: any) => u.shortenedURL === shotenedURLId);
+};
+
+export const getUrlByShortUrl = async (shortUrl: string) => {
+  return await UrlModel.findOne({ where : { shortUrl } });
 };
 
 
@@ -26,7 +33,7 @@ export const getUrlByShortUrl = async (shotenedURLId: string) => {
  * @param userId 
  * @returns 
  */
-export const createShortUrl = async (longURL: string, userId: number): Promise<IUrlModel | string> => {
+export const createShortUrlMock = async (longURL: string, userId: number): Promise<IUrlModel | string> => {
   let newShortURLId = await idGenerator();
   
   // check if already exists before generatoring
@@ -55,6 +62,43 @@ export const createShortUrl = async (longURL: string, userId: number): Promise<I
   return "failed to create new url";
 };
 
+/**
+ * Create new short url for user
+ * @param longURL 
+ * @param userId 
+ * @returns 
+ */
+export const createShortUrl = async (longUrl: string, userId: string): Promise<UrlModel | null> => {
+  let shortUrl = await idGenerator();
+  
+  // check if already exists before generatoring
+  const isExistingUrl = await getUrlByShortUrl(shortUrl);
+
+  if (isExistingUrl) {
+    // existing call new url with 8 letter
+    shortUrl = await idGenerator(8);
+  }
+
+  const newUrl: UrlModel = { 
+    longUrl,
+    shortUrl,
+    count: 0,
+    userId
+  } as UrlModel;
+
+  
+  console.log("NEW URL TO BE CREATED", newUrl);
+
+  const createdNewUrl = await UrlModel.create(newUrl);
+
+  if (createdNewUrl) {
+    console.log("CREATED NEW URL", createdNewUrl )
+    return createdNewUrl;
+  }
+    
+  return null;
+};
+
 
 /**
  * Get url by long name
@@ -76,25 +120,27 @@ export const getUrlByLongUrl = async (longURL: string, userId: number): Promise<
 };
 
 
-export const deleteByShortURLId = async (shortenedURLId:string, userId: number): Promise<boolean> => {
+export const deleteByShortUrlId = async (shortenedUrlId:string, userId: number): Promise<boolean> => {
   // confirm existing url
-  const isExistingURL = await getUrlByShortUrl(shortenedURLId);
+  const isExistingURL: any = await getUrlByShortUrl(shortenedUrlId);
 
   // if doesn't exist return true
-  if (!isExistingURL || isExistingURL.length === 0) return true;
+  if (!isExistingURL || !(isExistingURL.length > 0)) return true;
 
-  const isOwnedByLoggedInUser = isExistingURL[0].userId === userId;
-  if (!isOwnedByLoggedInUser) throw new Error("not own by user");
+  // const isOwnedByLoggedInUser = isExistingURL[0].userId === userId;
+  // if (!isOwnedByLoggedInUser) throw new Error("not own by user");
 
-  console.log("IS OWNED BY LOGGED IN USER", isOwnedByLoggedInUser)
+  // console.log("IS OWNED BY LOGGED IN USER", isOwnedByLoggedInUser)
 
   // if existing check belongs to user
-  if (isExistingURL && isOwnedByLoggedInUser) {
+  // if (isExistingURL && isOwnedByLoggedInUser) {
+  if (isExistingURL) {
     
     // TODO: replace with real db
-    const findData = await urlData.findIndex((u: any) => u.shortenedURL === shortenedURLId);
+    // const findData = await urlData.findIndex((u: any) => u.shortenedURL === shortenedURLId);
     
-    urlData.splice(findData, 1)
+    // urlData.splice(findData, 1)
+    
   
     return true;
   }
