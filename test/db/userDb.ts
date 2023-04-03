d1945dc76b58da758cd6b25441d40db22c822623
Nick <nsagias@gmail.com>
2023-04-03 15:06:18 -0400

    refactor mock db to test folder

---
+++ test/db/userDb.ts
@@ -0,0 +1,41 @@
+import bcrypt2 from "bcryptjs";
+import { IUser } from "../../types/user";
+
+export let users: IUser[] = [
+   {
+    id: 1,
+    firstName: "red",
+    lastName: "example",
+    email: "red@example.com",
+    emailVerified: new Date() as Date,
+    password: bcrypt2.hashSync("abc123", 10),
+    active: true,
+    deletedAt: undefined,
+    createdAt: new Date() as Date,
+    updatedAt: new Date() as Date,
+  },
+     {
+    id: 2,
+    firstName: "green",
+    lastName: "example",
+    email: "green@example.com",
+    password: bcrypt2.hashSync("abc123", 10),
+    active: true,
+    deletedAt: undefined,
+    createdAt: new Date() as Date,
+    updatedAt: new Date() as Date,
+  },
+ {
+    id: 3, 
+    firstName: "blue",
+    lastName: "example",
+    email: "blue@example.com",
+    password: bcrypt2.hashSync("abc123", 10),
+    active: true,
+    deletedAt: undefined,
+    createdAt: new Date() as Date,
+    updatedAt: new Date() as Date,
+  }
+];
+
+
