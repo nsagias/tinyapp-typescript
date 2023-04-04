9b53351fb856887b0120cb69a459d90413ef8646
Nick <nsagias@gmail.com>
2023-04-03 20:09:31 -0400

    update register route, remove duplicate code, update body, add null checks, update createUser params

--- routes/userRoute.ts
+++ routes/userRoute.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from "express";
 import { createUser, getUserByEmail } from "../DAL/userData";
-import { IUser } from "../types/user";
+import { IUser } from "../DAL/types/user";
 
 export const userRoute = Router();
 
@@ -14,53 +14,47 @@ userRoute.get("/users/:userId", (req: Request, res: Response) => {
 
 // userRoute.post
 userRoute.get("/register", async (req: Request, res: Response) => {
-  const myUserId = "815bd08a"; // mock user 
-  // const email = "red@example.com"; // existing user
-  const email = "fun@example.com"; // new user user
-  const password = "abc123";
-  const name = "bob";
   try {
+    const email = "bobEmail@example.com"; // new user user
+    const password = "abc123";
+    const firstName = "bobFirstName";
+    const lastName = "bobLastName";
     // Check for body 
-    // const name = req.body && req.body.name || null;
+
+    // const firstName = req.body && req.body.firstName || null;
+    // const lastName = req.body && req.body.lastName || null;
     // const email = req.body && req.body.email || null;
     // const password = req.body && req.body.password || null;
-    
-    if (!email || !password || !name) throw new Error("new_account_missing_information_1");
-    
 
     // check for empty strings
-    const parsedEmail = email.trim();
-    const parsedPassword = password.trim();
-    const parsedName = name.trim();
+    const parsedEmail = email && email.trim();
+    const parsedPassword = password && password.trim();
+    const parsedFirstName = firstName && firstName.trim();
+    const parsedLastName = lastName && lastName.trim();
 
-    if (parsedEmail === "" || parsedPassword === "" || parsedName === "") throw new Error("new_account_missing_information_2");
+    // TODO: add min legnth
+    if (!parsedEmail || !parsedPassword || !parsedFirstName || !parsedLastName) throw new Error("new_account_missing_information");
   
     // check if existing user
     const userExist: IUser | null = await getUserByEmail(parsedEmail);
 
-    if (userExist){
-
+    if (userExist) {
       await res.json({ message: "existing email on file"});
 
     } else  {
 
       // create new user
-      // const newUser = await createUser(parsedName, parsedEmail, parsedPassword);
-      // console.log("NEW USER", newUser);
-
-      // fiuser by email
-      
-      const newId: IUser | null = await getUserByEmail(parsedEmail);
-
-      if (!newId) throw new Error("User was not created")
+      const newUser = await createUser(parsedFirstName, parsedLastName ,parsedEmail, parsedPassword);
+      // check if user was created
+      if (!newUser) throw new Error("User was not created")
       
       // set token/cookie to new user id
-      await res.cookie("userID", newId);
-  
-      
+      // token string to get created here
+      // await res.cookie("userID", newId);
+
       // return new user data 
       // TODO: add DTO
-      await res.json({ name: parsedName, userId: newId, email: parsedEmail});
+      await res.json({ firstName: newUser.firstName,  lastName: newUser.lastName, isActive: newUser.active, token: "token string to go here"});
     }
         
   } catch (error: any) {
