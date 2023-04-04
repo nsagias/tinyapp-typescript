99c540a0603598ec0c2fce0ec1cdad460fb3f81c
Nick <nsagias@gmail.com>
2023-04-03 22:48:46 -0400

    refactor login section

--- routes/loginRoute.ts
+++ routes/loginRoute.ts
@@ -1,44 +1,25 @@
 import { Router, Request, Response } from "express";
-import { getUserByEmail } from "../DAL/userData";
-import { IUser } from "../DAL/types/user";
-import { checkPassword } from "../services/authService";
+import { login } from "../services/authService";
 
 export const loginRoute = Router();
 
 
 // app.post
 loginRoute.get("/login", async(req: Request, res: Response) => {
-  const myUserId = "1";
   try {
     // Check for body 
-    // const email = req.body && req.body.email || null;
-    // const password = req.body && req.body.password || null;
     const email = "red@example.com";
     const password = "abc123";
-    if (!email || !password) {
-      throw new Error("missing_email_or_password_1");
-    }
+    // const email = req.body && req.body.email || null;
+    // const password = req.body && req.body.password || null;
+ 
+    const parsedEmail = email && email.trim();
+    const parsedPassword = email && password.trim();
 
-    // check for empty strings
-    const parsedEmail = email.trim();
-    const parsedPassword = password.trim();
-
-    if (parsedEmail === "" || parsedPassword === "") {
-      throw new Error("missing_email_or_password_2");
-    }
-
-    const user: IUser | null = await getUserByEmail( parsedEmail || "red@example.com");
-    console.log("*USER",user);
-
-    const userAuth: boolean = await checkPassword(parsedEmail || "red@example.com", parsedPassword || "abc123");
-    console.log("***Auth:", userAuth);
-
-    if (!user) {
-      throw new Error("missing_email_or_password_3");
-    } else if (user) {
-      await res.cookie("userID", myUserId);
-      await res.json({ name: "nick", userId: myUserId, email: user, userAuth});
-    }
+    if (!parsedEmail || !parsedPassword) throw new Error("Login error");
+    
+    // login section
+    return await login(parsedEmail, parsedPassword);
         
   } catch (error: any) {
       console.error("ERROR",error);
