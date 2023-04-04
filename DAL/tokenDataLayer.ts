55b7c9f60cb97a7e50d50cf044253360c644f10d
Nick <nsagias@gmail.com>
2023-04-04 00:27:54 -0400

    create access token

--- DAL/tokenDataLayer.ts
+++ DAL/tokenDataLayer.ts
@@ -14,3 +14,37 @@ export const getTokenById = async (id: number ): Promise<IToken | null> => {
   return await Token.findOne({ where: { id} });
 };
 
+
+/**
+ * Create new access token and dave to db
+ * @param user 
+ * @param ip 
+ * @returns token or null
+ */
+export const createAccessToken = async (user: any, ip: string ): Promise<IToken| null> => {
+  // get auth secret and issuer from .env
+  const authSecret = process.env.AUTH_SECRET || null;
+  const issuer = process.env.TOKEN_ISSUER || null;
+
+  // Return null if no auth token
+  if (!authSecret || !issuer) return null;
+
+  // Create signed auth token
+  const authToken = await jwt.sign(user, authSecret, {
+    algorithm: "HS256",
+    issuer,
+    subject: user.id
+  });
+
+  // new token to be saved
+  const newTokenRecord = { authToken, ip, userId: user.id };
+
+  // Create new record
+  const createdTokenRecord = Token.create(newTokenRecord);
+
+  // Check if record created
+  if (!createdTokenRecord) return null;
+
+  // return created token
+  return createdTokenRecord;
+};
