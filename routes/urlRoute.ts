2223d6cb41a468e5b5e04f7ccdd2dea84f50660a
Nick <nsagias@gmail.com>
2023-04-05 23:03:59 -0400

    refactor error messages

--- routes/urlRoute.ts
+++ routes/urlRoute.ts
@@ -1,78 +1,125 @@
 import { Router, Request, Response } from "express";
-import { createShortUrl, deleteByShortUrl, getUrlByShortUrl, getUrlByLongUrl, getUrlsByUserId, updateUrlById }   from "../DAL/urlData";
-import { UrlModel } from "../models";
+import { createShortUrl, deleteByShortUrl, getUrlByShortUrl, getUrlByLongUrl, getUrlsByUserId}   from "../DAL/urlData";
+
 
 export const urlRoute = Router();
 
 /**
- * Redirect redirect by shortUrl
- */
-urlRoute.get("/u/:shortUrl", async (req: Request, res: Response) => {
-  try {
-
-    const shortUrl = req.params && req.params.shortUrl || null;
-
-    if (!shortUrl) {
-      throw new Error("please provide shortened Url")
-    }
-
-    // receives a shoten url from an anonymous user
-    const longUrlData: UrlModel  = await getUrlByShortUrl(shortUrl, null) as UrlModel;
-
-    // check if url exist
-    if (!longUrlData) throw new Error("URL does not exist")
-    
-    // update count + 1
-    await updateUrlById(longUrlData.id, { count: longUrlData.count + 1 });
-    
-    res.redirect(longUrlData.longUrl);
-
-  } catch (error: any) {
-    console.error(error);
-    res.json({ message: "error"});
-  }
-});
-
-
-/**
  * Get urls for user
  */
-urlRoute.get("/u/:userId", async(req: Request, res: Response) => {
+urlRoute.get("/urls/:userId", async(req: Request, res: Response) => {
+
+  const errorMessage = "Missing information for user";
+
   try {
-    // 
+    const ip = "127.0.0.1";
+    // const ip = req.socket && req.socket?.remoteAddress && req.socket?.remoteAddress.split("::ffff:")[1] || null;
+    if (!ip) throw new Error(errorMessage);
+
     const userId = req.params && req.params.userId && parseInt(req.params.userId, 10) || null;
     const cookieUserId = req.cookies && req.cookies.userID && parseInt(req.cookies.userID, 10) || null;
   
-    if (!userId) throw new Error("missing user params");
-    if (!cookieUserId) throw new Error("please authenticate")
+    if (!userId) throw new Error(errorMessage);
+    if (!cookieUserId) throw new Error(errorMessage);
 
     const urls = await getUrlsByUserId(cookieUserId);
-    // if (!urls || !(urls.length > 0)) return res.json({ message: "seccuess", data: null})
-
+   
     // TODO: DTO
     res.json({ message: "success",  data: urls });
     
   } catch (error: any) {
     console.error(error);
-    res.json({ message: "error"});
+    res.json({ message: errorMessage});
+  }
+});
+
+
+/**
+ * Get url by shortendid
+ * no validation
+ */
+urlRoute.get("/urls/:shortUrl", async (req: Request, res: Response) => {
+
+  const errorMessage = "Missing information for url";
+
+  try {
+    const ip = "127.0.0.1";
+    // const ip = req.socket && req.socket?.remoteAddress && req.socket?.remoteAddress.split("::ffff:")[1] || null;
+    if (!ip) throw new Error(errorMessage);
+
+    const shortUrl = req.params && req.params.shortUrl || null;
+    const cookieUserId = req.cookies && req.cookies.userID && parseInt(req.cookies.userID, 10) || null;
+
+
+    if (!shortUrl)  throw new Error(errorMessage);
+    if (!cookieUserId) throw new Error(errorMessage);
+    
+    // receives a shoten url from an anonymous user
+    const longURLData = await getUrlByShortUrl(shortUrl, null)
+        
+    // TODO: add DTO
+    res.json({ message: "success", data: longURLData});
+
+  } catch (error: any) {
+    console.error(error);
+    res.json({ message: errorMessage });
+  }
+});
+
+/**
+ * Get url by shortendid
+ * no validation
+ * udpate path
+ */
+urlRoute.post("/urls/update", async (req: Request, res: Response) => {
+
+  const errorMessage = "Missing information for update";
+
+  try {
+    const ip = "127.0.0.1";
+    // const ip = req.socket && req.socket?.remoteAddress && req.socket?.remoteAddress.split("::ffff:")[1] || null;
+    if (!ip) throw new Error(errorMessage);
+    
+    const shortUrl= req.body && req.body.shortUrl || null; // use body from form
+    const cookieUserId = req.cookies && req.cookies.userID && parseInt(req.cookies.userID, 10) || null;
+
+
+    if (!shortUrl)  throw new Error(errorMessage);
+    if (!cookieUserId) throw new Error(errorMessage);
+    
+    // receives a shoten url from an anonymous user
+    const longURLData = await getUrlByShortUrl(shortUrl, null)
+    if (!longURLData) throw new Error(errorMessage);
+        
+    // TODO: add DTO
+    res.json({ message: "success", data: longURLData});
+
+  } catch (error: any) {
+    console.error(error);
+    res.json({ message: errorMessage});
   }
 });
 
 
 
-
 // urlRoute.post
-urlRoute.get("/u/delete", async (req: Request, res: Response) => {
+urlRoute.get("/urls/delete", async (req: Request, res: Response) => {
+
+  const errorMessage = "Missing information for deleting url";
+
   try {
     // TODO: update before
-    // const longURL = req.body && req.body.shortURId|| null; // use body from form
-    const shortURLId = req.params && req.params.shortUrlId || null;
+    const ip = "127.0.0.1";
+    // const ip = req.socket && req.socket?.remoteAddress && req.socket?.remoteAddress.split("::ffff:")[1] || null;
+    if (!ip) throw new Error(errorMessage);
+
+    const shortUrl= req.body && req.body.shortUrl || null; // use body from form
     const userData = req.cookies && req.cookies.userID && parseInt(req.cookies.userID) || null;
 
-    if (!shortURLId)  throw new Error("please provide shortened Url");
-    if (!userData) throw new Error("please authenticate");
+    if (!shortUrl)  throw new Error(errorMessage);
+    if (!userData) throw new Error(errorMessage);
 
-    const isDeleted = await deleteByShortUrl(shortURLId, userData.id);
+    const isDeleted = await deleteByShortUrl(shortUrl, userData.id);
 
     if (isDeleted) return res.json({ message: true});
     
@@ -80,7 +127,7 @@ urlRoute.get("/u/delete", async (req: Request, res: Response) => {
  
   } catch (error: any) {
     console.error(error);
-    res.json({ message: "error"});
+    res.json({ message: errorMessage});
   }
 });
 
@@ -88,32 +135,38 @@ urlRoute.get("/u/delete", async (req: Request, res: Response) => {
 
 // urlRoute.post
 // new routes urls routes
-urlRoute.get("/u/new", async (req: Request, res: Response) => {
+urlRoute.get("/urls/new", async (req: Request, res: Response) => {
+
+  const errorMessage = "Missing information for creating short url";
+
   try {
-    // TODO: update before
-    // const longURL = req.body && req.body.longURL || null;
+    const ip = "127.0.0.1";
+    // const ip = req.socket && req.socket?.remoteAddress && req.socket?.remoteAddress.split("::ffff:")[1] || null;
+    if (!ip) throw new Error(errorMessage);
+
     const longURL = req.params && req.params.longUrl || null; // do not use params
+    // const longURL = req.body && req.body.longURL || null;
     const userData = req.cookies && req.cookies.userID  && parseInt(req.cookies.userID, 10) || null; // update with token info
 
     // t oken info
-    if (!longURL)  throw new Error("please provide shortened Url");
-    if (!userData) throw new Error("please authenticate");
+    if (!longURL)  throw new Error(errorMessage);
+    if (!userData) throw new Error(errorMessage);
 
     // check if long name already exists
     const existingLongURL = await getUrlByLongUrl(longURL, userData.id);
 
-    if (existingLongURL)  throw new Error("existing url");
+    if (existingLongURL)  throw new Error(errorMessage);
     
     // receives a shoten url from an anonymous user
     const longURLData = await createShortUrl(longURL, userData.id);
-    if (!longURLData ) throw new Error("URL was not created");
+    if (!longURLData ) return res.json({ message: errorMessage});
         
     // TODO: add DTO
     res.json({ message: "success", data: longURLData});
 
   } catch (error: any) {
     console.error(error);
-    res.json({ message: "error"});
+    res.json({ message: errorMessage});
   }
 });
 
