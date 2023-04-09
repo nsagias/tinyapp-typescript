9db1f34562b2cc94020ba123b15dc828bd33b1f9
Nick <nsagias@gmail.com>
2023-04-09 16:48:54 -0400

    add token authentication root

--- routes/authRoute.ts
+++ routes/authRoute.ts
@@ -1,22 +1,28 @@
 import { Router, Request, Response } from "express";
+import { IToken } from "../DAL/types/token";
+import { authenticateTokenUser } from "../services/authService";
 
 export const authRoute = Router();
 
-
-// TODO: app.post
-// TODO: to update with token authentication
-authRoute.get("/authenticate", async(req: Request, res: Response) => {
-  // const ip = "127.0.0.1";
-  const ip = req.socket && req.socket?.remoteAddress && req.socket?.remoteAddress.split("::ffff:")[1] || null;
-  if (!ip) throw new Error("Athentication Error");
-
-  const myUserId = 1;
-  const cookieCheck = req.cookies && req.cookies.userID || null;
-  if (cookieCheck === myUserId) {
-    res.json({ name: "nick"});
-  } else {
-    res.json(null);
+authRoute.post("/authenticate", async(req: Request, res: Response) => {
+  const errorMessage = "Athentication Error";
+  try {
+    const ip = req.socket && req.socket?.remoteAddress && req.socket?.remoteAddress.split("::ffff:")[1] || null;
+    const authToken = await req.body && req.body.token || null;
+    const userId = await req.body && req.body.userId || null;
+    if (!ip) throw new Error(`${errorMessage} 1`);
+    if (!userId) throw new Error(`${errorMessage} 2`);
+    if (!authToken) return new Error(`${errorMessage} 3`);
+  
+    const userData: IToken = await authenticateTokenUser(userId.toString(), ip, authToken) as IToken;
+    if (userData) return res.json({ message: "success"});
+    return res.json({ message: "Please login again"})
+    
+  } catch (error: any) {
+    console.error(error);
+    res.json({ message: `${errorMessage} 4`});
   }
+
 });
 
 
